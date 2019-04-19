258b58e7816cd2ec077c0c7dbccaf5c8781c28b4
jdcanado <jdcanado@gmail.com>
2019-04-19 20:33:31 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
 #database.py
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
-engine = create_engine('postgres://{}:{}@{}/{}'.format(username, password, host, databasename), convert_unicode=True)
-metadata = MetaData()
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'db.apitruckpad')
 def init_db():
   metadata.create_all(bind=engine)
