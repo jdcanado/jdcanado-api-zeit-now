528652d977f8b21dffd4722edc691d2cfe81a2c9
jdcanado <jdcanado@gmail.com>
2019-04-20 00:18:18 -0300

    Update models.py

--- models.py
+++ models.py
@@ -23,10 +23,7 @@ class Caminhao(object):
     
     def __init__(self, id=None, tipo=None):
         self.id = id
-        self.tipo = tipo
-        
-    def add(self)
-        db_session.add(self)        
+        self.tipo = tipo           
         
 caminhao = Table('caminhao', metadata,
     Column('id', Integer, primary_key=True),
