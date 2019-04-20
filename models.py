11186ec96520b216145540ea53df453260e10b40
jdcanado <jdcanado@gmail.com>
2019-04-20 14:46:41 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 # models.py
+
 from sqlalchemy import Table, Column, Integer, Text
+
 from sqlalchemy.orm import mapper
+
 from database import metadata, db_session
 
 class BlogPost(object):
     query = db_session.query_property()
+
     def __init__(self, id=None, title=None, post=None):
         self.id = id
         self.title = title
@@ -20,11 +24,15 @@ mapper(BlogPost, blog_posts)
 
 class Caminhao(object):
     query = db_session.query_property()
-    
+
     def __init__(self, id=None, tipo=None):
         self.id = id
         self.tipo = tipo
-           
+
+    def add(self):
+        db_session.add(self)
+        db_session.commit
+
 caminhao = Table('caminhao', metadata,
     Column('id', Integer, primary_key=True),
     Column('tipo', Text)
