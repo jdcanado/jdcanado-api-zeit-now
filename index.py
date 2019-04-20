a5ddf4d48124988b012fb73acdbc68f408689ec2
jdcanado <jdcanado@gmail.com>
2019-04-20 02:42:25 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
 # app.py
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask
 from flask_restplus import Resource, Api, fields
 from werkzeug.contrib.fixers import ProxyFix
 from database import db_session
 from models import BlogPost
-from flask_sqlalchemy import SQLAlchemy 
-from flask_marshmallow import Marshmallow 
+from models import Caminhao
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -35,34 +34,15 @@ class BlogPosts(Resource):
     def get(self, **kwargs):
         return BlogPost.query.all()
 
-class Caminhao(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  tipo = db.Column(db.String(100), unique=True)
-  
-  def __init__(self, tipo):
-    self.tipo = tipo
-    
-# Caminhao Schema
-class CaminhaoSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'tipo')
-
-# Init schema
-caminhao_schema = CaminhaoSchema(strict=True)
-caminhoes_schema = CaminhaoSchema(many=True, strict=True)
-
-@app.route('/caminhao', methods=['POST'])
-def adicionar_caminhao():
-  if not request.json or not 'tipo' in request.json:
-        abort(400)
-
-  tipo = request.json['tipo']
-  
-  novo_caminhao = Caminhao(tipo)
-  db.session.add(novo_caminhao)
-  db.session.commit()
-
-return caminhao_schema.jsonify(novo_caminhao)
+@api.route('/caminhoes')
+class Caminhao(Resource):
+    model = api.model('Caminhao', {
+        'id': fields.Integer,
+        'tipo': fields.String,
+    })
+    @api.marshal_with(model, envelope='resource')
+    def get(self, **kwargs):
+        return Caminhao.query.all()
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
